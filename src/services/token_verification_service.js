fff0e9654c13185d306c11467940de60c7d26f2b
Danial-A <ahmeddanial990@gmail.com>
2021-11-26 11:29:46 +0500

    Demo change from danial-a

--- src/services/token_verification_service.js
+++ src/services/token_verification_service.js
@@ -3,3 +3,7 @@ const {verification_tokens} = require('../../models');
 module.exports.get_token = async (token) =>{
     return await verification_tokens.findOne({where : {token}});
 }
+
+module.exports.demo_function =() =>{
+    console.log("Hello world")
+}
